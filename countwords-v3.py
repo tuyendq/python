573ba9e0919da4644de192f470d619ff1aa69488
Tuyen Dang <tuyendq@gmail.com>
2018-03-05 12:09:31 +0700

    Add countwords*.py

---
+++ countwords-v3.py
@@ -0,0 +1,6 @@
+from collections import Counter
+# python v2
+# from io import open
+
+print(Counter(open("tngh.txt").read()))
+
---
+++ countwords.py
@@ -0,0 +1,5 @@
+from collections import Counter
+#from io import open
+
+print(Counter(open("tngh.txt").read()))
+
