889c9d9ec1754857d97126da1cf103f93d0e0752
Tuyen Dang <tuyendq@gmail.com>
2019-04-18 11:29:06 +0700

    t420: edit funcmath.py

--- funcmath.py
+++ funcmath.py
@@ -2,8 +2,13 @@ def square(n):
 	"""Square a number."""
 	return n * n
 
+def power(base, exponent):
+	"""Raise base to the power of exponent."""
+	return base ** exponent
 
 # Examples
 # square
 number = float(raw_input("Enter a number: "))
 print "Square of %r is: %f." % (number, square(number))
+
+print "%r ** %r is: %f." % (number, number, power(number, number))
