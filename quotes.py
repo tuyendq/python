30fa1bba6a7ed21d78a391030c1915364f17ef82
Tuyen Dang <tuyendq@gmail.com>
2019-04-22 11:04:40 +0700

    t420 ubuntu: modify quotes.py

--- quotes.py
+++ quotes.py
@@ -5,7 +5,7 @@ import csv
 script, file_name, file_html = argv
 
 # test first
-print "arg1: %r, arg2: %r, arg3: %r" % (script, file_name, file_html)
+# print "arg1: %r, arg2: %r, arg3: %r" % (script, file_name, file_html)
 
 # Todo: add exception
 with open(file_name, 'r') as input_file:
@@ -18,7 +18,6 @@ with open(file_name, 'r') as input_file:
 
 	print """===Print a random quote==="""
 	# random_number = random.randint(1, 8)
-	print contents
 
 	# qotd = contents[random_number]
 	
@@ -26,12 +25,38 @@ with open(file_name, 'r') as input_file:
 	# for i in range(1, random_number):
     #	input_file.readline()
 
-#qotd =  input_file.readline()
-#print qotd
-
-#input_file.close()
 
 
-output_file = open(file_html, 'w')
-#output_file.write(qotd)
-output_file.close()
+
+def create_html(file_html, quote_content, quote_author):
+		html_content = """
+		<!DOCTYPE html>
+		<html>
+		<head>
+		<meta charset="utf-8">
+		<title>What, Why, and How to practice habit?</title>
+		<link rel="stylesheet" href="styles.css">
+		</head>
+
+		<body>
+		<center>
+		<blockquote>{quote_content}</blockquote>
+		<author>&mdash; {quote_author}</author>
+		</center>
+		</bofy>
+
+		</html>
+
+		"""
+		html_content = html_content.format(quote_content=quote_content, quote_author=quote_author)
+
+		output_file = open(file_html, 'w')
+		output_file.write(html_content)
+		output_file.close()
+
+
+quote_content = "Everything is practice"
+quote_author = "Pele"
+
+create_html(file_html, quote_content, quote_author)
+
