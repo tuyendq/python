b98bf37f0781fa0584cb201ad65198de4f67701d
Tuyen Dang <tuyendq@gmail.com>
2020-09-06 21:01:42 +0700

    add more examples

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,10 +1,15 @@
 # Use list comprehension and "any" built-in function to print prime numbers in range(2,1000) 
 
-prime_numbers = []
-for i in range(2,1000):
-    if any([i % j == 0 for j in range(2,i-1)]): # use list comprehension and any function
-        continue
-    else:
-        prime_numbers.append(i)
+def main():
+    prime_numbers = []
+    for i in range(2,1000):
+        if any([i % j == 0 for j in range(2,i-1)]): # use list comprehension and any function
+            continue
+        else:
+            prime_numbers.append(i)
 
-print(prime_numbers)
+    print(prime_numbers)
+
+
+if __name__ == "__main__":
+    main()
