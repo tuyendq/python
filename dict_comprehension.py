829e384f2845d70280934e1259ae99ca6b45c622
Tuyen Dang <tuyendq@gmail.com>
2020-09-07 18:52:51 +0700

    add dictionary comprehension

---
+++ dict_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# File: dict_comprehension.py
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create dict comprehension: new_fellowship
+new_fellowship = {member: len(member) for member in fellowship}
+
+# Print the new dictionary
+print(new_fellowship)
