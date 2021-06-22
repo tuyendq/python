76f4cc4b80c7cde854c2b381d4e89b1cbf7ad996
Tuyen Dang <tuyendq@gmail.com>
2021-06-22 14:45:19 +0700

    mp3-dl.py: check request status code

--- webscraping/mp3-dl.py
+++ webscraping/mp3-dl.py
@@ -14,15 +14,22 @@ def downloadmp3(url):
     print("URL: ", url)
     mp3Filename = getFilename(url)
     print("Start downloading ", mp3Filename, " ...")
-    result = r.get(url, allow_redirects=True, verify=False)
-    open(mp3Filename, 'wb').write(result.content)
-    print("Finish downloading ", mp3Filename, " ...")
+    # result = r.get(url, allow_redirects=True, verify=False)
+    # result = r.get(url, allow_redirects=True)
+    result = r.get(url)
+    if result.status_code == 200:    
+        print("Header: ", result.headers)
+        print("Saving to file: ", mp3Filename)
+        open(mp3Filename, 'wb').write(result.content)
+        print("Finish downloading ", mp3Filename, " ...")
+    else:
+        print("Error occurred. Status code: ", result.status_code, result.reason)
 
 def download(filename):
     """Download list of mp3 file in a file, line-by-line"""
     with open(filename) as file:
         for url in file:
-            print(url)
+            print("Source URL:", url)
             downloadmp3(url)
 
 def main():
