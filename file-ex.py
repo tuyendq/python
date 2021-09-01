d0c0f6db1a65196321f1f7ffb559c0cb921e5ce4
Tuyen Dang <tuyendq@gmail.com>
2021-09-01 22:21:11 +0700

    file-ex.py: list files only, list dir only

--- file-ex.py
+++ file-ex.py
@@ -1,5 +1,14 @@
 # Working with files
+# LinkedIn Learning course: Working with Files by Kathryn Hodge
+from datetime import datetime
 import os
+
+
+def format_datetime(timestamp):
+    '''Format date time'''
+    utc_timestamp = datetime.utcfromtimestamp(timestamp)
+    formated_date = utc_timestamp.strftime("%d %b %Y %H %M %S")
+    return formated_date
 
 
 def show_cwd():
@@ -13,15 +22,38 @@ def up_1level():
     os.chdir("../")
 
 
-def list_dir(directory):
+def list_all(directory):
     '''List files and folders'''
     with os.scandir(directory) as entries:
         for entry in entries:
-            print(entry.name)
+            print(entry.name, end='\t')
+            info = entry.stat()
+            print("Created time: ", format_datetime(info.st_ctime))
+            print("Last access time: ", format_datetime(info.st_atime))
+            print("Size: ", info.st_size)
+
+
+def list_dir(directory):
+    '''List directories only'''
+    with os.scandir(directory) as entries:
+        for entry in entries:
+            if entry.is_dir():
+                print("Directory: ", entry.name)
+
+
+def list_file(directory):
+    '''List files only'''
+    with os.scandir(directory) as entries:
+        for entry in entries:
+            if entry.is_file():
+                print("File: ", entry.name)
 
 
 if __name__ == "__main__":
     show_cwd()
     up_1level()
     show_cwd()
+    list_all("./")
+    list_all("F:/Projects/github/python")
     list_dir("./")
+    list_file("F:/Projects/github/python")
