2dcb557e685cbcb9c1750d3ee48b3dc55078fc1f
Tuyen Dang <tuyendq@gmail.com>
2023-02-10 20:56:39 +0700

    update countwords-v3.py

--- countwords-v3.py
+++ countwords-v3.py
@@ -1,6 +1,19 @@
+#!/usr/bin/env python
+
+# print("Count words frequency in text file")
+
 from collections import Counter
 # python v2
 # from io import open
 
-print(Counter(open("tngh.txt").read()))
+# print(Counter(open("tngh.txt","r", encoding="utf-8").read()))
+result = Counter(open("tngh.txt","r", encoding="utf-8").read())
+
+outfile = "tngh_freq.csv"
+# print(outfile)
+with open(outfile, mode="w", encoding="utf-8", newline="") as fp:
+    # Write header line
+    fp.write("Word,Frequency\n")
+    for char, count in result.items():
+        fp.write("{},{}\n".format(char,count))
 
