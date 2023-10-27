8ad5cab2aae08d8b68afcf97e8b8a08f64c5a467
Tuyen Dang <tuyendq@gmail.com>
2023-10-27 23:35:35 +0700

    add static method to class

--- 101_class.py
+++ 101_class.py
@@ -8,21 +8,43 @@ print(p.x)
 print(p.y)
 
 class Book:
-    def __init__(self, title):
+    def __init__(self, title, author, price):
         self.title = title
+        self.author = author
+        self.price = price
 
-book1 = Book("The Power of Now")
+    def get_price(self):
+        return self.price
+
+book1 = Book("The Power of Now", "Eckhart Tolle", 9.99)
 print(book1.title)
+print(book1.author)
+print(book1.get_price())
+
+print(type(book1))
+print(isinstance(book1, Book))  # Should be True
+print(isinstance(book1, Point))  # Should be False
 
 class Person():
     def __init__(self, firstname):
         self.firstname = firstname
-        # Just a convention
+        # Just a convention for hidden attribute, but you can hack it
         self.__secret = "This is a secret attribute! But you can hack it."
+
+    """One way to create a static method"""
+    @staticmethod
+    def get_person():
+        print(">>> What is static method used for?")
+
+
+# Go ahead, just call static method without creating an instance
+print(Person.get_person())
 
 person1 = Person("Tuyen")
 print(person1.firstname)
 
-print(person1._Person__secret)
-print(person1.__secret)
+
+
+print(person1._Person__secret)  # You can hack hidden attribute
+print(person1.__secret)  # Should get AttributeError error (no __secret attr)
 
