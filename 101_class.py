e82b8a9ab302ca7c8f5b86abafb7fd378b36acd5
Tuyen Dang <tuyendq@gmail.com>
2023-10-27 21:43:12 +0700

    101_class.py: add Book, Person

--- 101_class.py
+++ 101_class.py
@@ -6,3 +6,17 @@ class Point:
 p = Point(3, 5)
 print(p.x)
 print(p.y)
+
+class Book:
+    def __init__(self, title):
+        self.title = title
+
+book1 = Book("The Power of Now")
+print(book1.title)
+
+class Person():
+    def __init__(self, firstname):
+        self.firstname = firstname
+
+person1 = Person("Tuyen")
+print(person1.firstname)
