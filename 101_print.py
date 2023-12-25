d63091e6edb30e34cad992f3bf3c398309f9d993
Tuyen Dang <tuyendq@gmail.com>
2023-12-25 15:35:20 +0700

    update 101_print.py

--- 101_print.py
+++ 101_print.py
@@ -7,3 +7,11 @@ print("Monty", "Python", end="-")  # Default end="\n" - new line
 print()
 # Using sep keyword
 print("Guido", "Van", "Rossun", sep="-")  # Default sep=" " - space
+
+# Using both sep and end keywords
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+#
+print("hello!" * 5)
+
