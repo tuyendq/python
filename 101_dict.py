446750bf7487500e3776fe8c5e80548efa226f10
Tuyen Dang <tuyendq@gmail.com>
2025-04-04 21:21:24 +0700

    x230: update 101_dict.py

--- 101_dict.py
+++ 101_dict.py
@@ -1,3 +1,4 @@
+# Create an empty dictionary
 cusip_lookup = {}
 
 # Yet another way to create a dictionary
@@ -6,6 +7,17 @@ another_cusip_lookup = dict()
 print(cusip_lookup)
 
 print(another_cusip_lookup)
+
+
+# Dictionary with keys and values
+cusip_lookup = {'123': 'AAPL', '456': 'MSFT', '789': 'GOOG'}
+
+
+# Dictionary methods
+print(cusip_lookup.keys())  # Print all keys in the dictionary
+print(cusip_lookup.values())  # Print all values in the dictionary
+
+
 
 
 
